a21c66e493a127d56a156eeedd3e63c2992c9c01
Lucas Kaíque <lucas.kaique175@hotmail.com>
2021-08-25 10:48:35 -0300

    navbar quase pronta

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,11 +6,8 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden bg-gray-100">
-      <main className="flex-grow">
-        <h1 className="text-5xl font-bold">Navbar</h1>
-        <h1 className="text-5xl font-bold">Header</h1>
-        <h1 className="text-5xl font-bold">Questions</h1>
-        <h1 className="text-5xl font-bold">Footer</h1>
+      <main>
+        <h1 className="text-5xl font-bold">CONTENT</h1>
       </main>
     </div>
   );
